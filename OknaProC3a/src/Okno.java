87696778569de1e77fd9899ba009d794ac84294a
Alena <Alena@10.6.2.171>
2018-03-20 08:09:25 +0100

    change title

--- OknaProC3a/src/Okno.java
+++ OknaProC3a/src/Okno.java
@@ -35,7 +35,7 @@ public class Okno extends JFrame {
 	 * Create the frame.
 	 */
 	public Okno() {
-		setTitle("Titulek");
+		setTitle("Novy titulek");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
